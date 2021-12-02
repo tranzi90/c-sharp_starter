523aa3742bc8c3a25481cfa135bfb71398ebb88b
denis_levchenko <tranzi90@gmail.com>
2021-12-02 20:20:46 +0300

    Init

---
+++ c-sharp_starter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_starter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
