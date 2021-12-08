5847c665733c535770fa7e5ebd8e15a50613598c
denis_levchenko <tranzi90@gmail.com>
2021-12-08 20:21:56 +0300

    method overloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Operation() { Console.WriteLine("Метод без аргрументов"); }
+
+        static void Operation(string argument) { Console.WriteLine(argument); }
+
+        static void Operation(int argument) { Console.WriteLine(argument); }
+
+        static void Operation(string argument1, int argument2) { Console.WriteLine(argument1 + argument2); }
+
+        static void Main()
+        {
+            Operation();
+            Operation("text");
+            Operation(123);
+            Operation("text", 123);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
