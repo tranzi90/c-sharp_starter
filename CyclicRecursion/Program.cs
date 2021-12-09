1374733fe84a51727f4b9a638d278756717f60d1
denis_levchenko <tranzi90@gmail.com>
2021-12-09 20:20:53 +0300

    recursion

---
+++ CyclicRecursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CyclicRecursion
+{
+    class Program
+    {
+        static void ProcedureA()
+        {
+            Console.WriteLine("A");
+            ProcedureB();
+        }
+        static void ProcedureB()
+        {
+            Console.WriteLine("B");
+            ProcedureC();
+        }
+        static void ProcedureC()
+        {
+            Console.WriteLine("C");
+            ProcedureA();
+        }
+
+        static void Main()
+        {
+            ProcedureA();
+        }
+    }
+}
