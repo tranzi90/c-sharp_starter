b3818e5bb8e66a6c539e9cc18a12ec250bb466c8
denis_levchenko <tranzi90@gmail.com>
2021-12-11 20:11:47 +0300

    OOP

---
+++ 000_MyFirstOOProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyFirstOOProgram
+{
+    class MyClass
+    {
+        public int field;
+
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass a = new MyClass();
+            a.field = 1;
+            Console.WriteLine(a.field);
+            a.Method();
+
+            MyClass b = new MyClass();
+            b.field = 2;
+            Console.WriteLine(b.field);
+            b.Method();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
