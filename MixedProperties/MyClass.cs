2cb29482b92afcce2f9d6a611cc08d8fa44783cf
denis_levchenko <tranzi90@gmail.com>
2021-12-12 21:18:35 +0300

    all types of properties in objects

---
+++ MixedProperties/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MixedProperties
+{
+    class MyClass
+    {
+        int field = -1;
+
+        public int Property_1
+        {
+            get => (field < 0) ? default : field; // getter (accessor)
+            set { field = value; }                // setter (mutator)
+        }
+
+        public int Property_2
+        {
+            get { return (field < 0) ? default : field; } // getter (accessor)
+            set => field = value;                         // setter (mutator)
+        }
+    }
+}
